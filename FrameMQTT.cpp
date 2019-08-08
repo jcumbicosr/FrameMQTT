821a2a2e6517d87a67d7f5840985d257d2a89e49
jcumbicosr <52890398+jcumbicosr@users.noreply.github.com>
2019-08-08 09:27:31 -0500

    Initial commit

---
+++ FrameMQTT.cpp
@@ -0,0 +1,361 @@
+/*
+ *
+ * 
+ *  WaspMQTTClient.cpp
+ *
+ *  The WaspMQTTClient library implements the MQTT protocol to work
+ *  with the waspmote's 3G module. There were no such implementation
+ *  for waspmote, although the MQTT protocol is a very good solution,
+ *  especially for remote, low energy cost applications with unreliable
+ *  network connection.
+ *
+ *  Version:		1.0
+ *
+ *  Design:		Glaros Anastasios (glrs)
+ *
+ *  Implementation:	Glaros Anastasios (glrs)
+ *
+*/
+
+
+/*
+ *  TODO: Implement unsubscribe function
+*/
+
+#include "FrameMQTT.h"
+
+//version 2.0.0
+
+/* - - - - - - - CONSTRUCTORS - - - - - - - */
+
+FrameMQTT::FrameMQTT() {
+	_id_max = 23;
+}
+
+
+/* * * * * * * * * * * * * * * * * */
+/* * *  MQTT CLIENT FUNCTIONS  * * */
+/* * * * * * * * * * * * * * * * * */
+
+/* - - - - - - CONNECT FUNCTIONS - - - - - - */
+
+void FrameMQTT::conectar( const char *id) 
+{
+	return conectar(id, NULL, NULL);
+}
+
+void FrameMQTT::conectar( const char *user, const char *key) 
+{
+	char clientID[9];
+	const char* const alphanum ="0123456789"
+	"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	"abcdefghijklmnopqrstuvwxyz";
+
+     
+	// Generate ClientID
+	for (int i = 0; i < 8; i++) {
+		clientID[i] = alphanum[random(51)];
+	}
+	clientID[8]='\0';
+	
+	return conectar(clientID);
+}
+
+void FrameMQTT::conectar(const char *id, const char *user, const char *key) 
+{
+	
+	uint8_t i = 0;
+	uint8_t k = 0;
+	uint8_t len_total = 0;
+	uint8_t id_length = strlen( id );
+	uint8_t flags = 2; //bit 1 -> clean session
+	// init buffer
+	memset( buffer, 0x00, sizeof(buffer) );
+	// init counter
+	length=0;
+	
+	//longitud de ID
+	if( id_length > _id_max)
+	{	
+		id_length = 23;
+	}
+	
+	//Longitud si existe User y Password
+	if(user != NULL)
+	{
+		k = strlen(user) + 2;  		// +2 bytes para longitud de 'user' 
+		flags = flags | 0x80;  		// bit 7 -> User flag
+		if(key != NULL)
+		{
+			k += strlen(key) + 2;	// +2 bytes para longitud de 'password' 
+			flags = flags | 0x40;	// bit 6 -> Password flag
+		}
+	}
+	buffer[length] = 16;
+	length++;
+	
+	len_total = 12 + id_length + k;		//Longitud total de trama
+	i = len_total/128;
+	if(i > 0)
+	{
+		buffer[length] = (len_total % 128) | 0x80;
+		length++;
+		buffer[length] = i;
+		length++;
+	}
+	else
+	{
+		buffer[length] = len_total;
+		length++;
+	}
+	
+	
+	buffer[length] = 0;
+	length++;
+	buffer[length] = 4;
+	length++;
+	buffer[length] = 'M';
+	length++;
+	buffer[length] = 'Q';
+	length++;
+	buffer[length] = 'T';
+	length++;
+	buffer[length] = 'T';
+	length++;
+	buffer[length] = 4;
+	length++;
+	buffer[length] = flags;		//Connect flags
+	length++;
+	buffer[length] = ((MQTT_KEEPALIVE) >> 8);
+	length++;
+	buffer[length] = ((MQTT_KEEPALIVE) & 0xFF);
+	length++;
+	
+	buffer[length] = 0;
+	length++;
+	buffer[length] = id_length;
+	length++;
+	
+	// Añadir client ID
+	for(i = 0; i < id_length ; i++)
+	{
+		buffer[length] = id[i];
+		length++;
+	}
+	
+	// Añadir User y Password
+	if(user != NULL)
+	{
+		
+		buffer[length] = 0;
+		length++;
+		buffer[length] = strlen(user);
+		length++;
+		for(i = 0; i < strlen(user) ; i++)
+		{
+			buffer[length] = user[i];
+			length++;
+		}
+		if(key != NULL)
+		{
+			
+			buffer[length] = 0;
+			length++;
+			buffer[length] = strlen(key);
+			length++;
+			for(i = 0; i < strlen(key) ; i++)
+			{
+				buffer[length] = key[i];
+				length++;
+			}
+			
+		}	
+	}
+
+}
+
+void FrameMQTT::pub_ThingSpeak(char *channel, char *apikey, char *body){
+	return pub_ThingSpeak(channel, apikey, (uint8_t*)body, strlen(body));
+/*	
+	uint8_t i = 0;
+	uint8_t  topic_length = 0;
+	uint8_t  body_length  = strlen( body );
+	uint8_t total_length = 0; //uint16_t
+	uint8_t x = 0;
+	char topic[50];
+	
+	// init buffer
+	memset( buffer, 0x00, sizeof(buffer) );
+	// init counter
+	length=0;
+	
+	snprintf(topic, sizeof(topic),"channels/%s/publish/%s", channel, apikey);
+	topic_length = strlen( topic );
+	
+	total_length = 2 + topic_length + body_length;
+	
+	buffer[0] = 48;
+	x = total_length/128;
+	if (x > 0){
+		length++;
+		buffer[length] = (total_length % 128) | 0x80;
+		length++;
+		buffer[length] = x;
+	}else{
+		length++;
+		buffer[length] = total_length;
+	}
+	
+	length++;
+	buffer[length] = 0;
+	length++;
+	buffer[length] = topic_length;
+	length++;
+	
+	//topic
+	for (i = 0; i < topic_length ; i++){
+		buffer[length] = topic[i];
+		length++;
+	}
+	
+	//body
+	for (i = 0; i < body_length ; i++){
+		buffer[length] = body[i];
+		length++;
+	}
+*/
+}
+
+void FrameMQTT::pub_ThingSpeak(char *channel, char *apikey, uint8_t *body, uint16_t len)
+{
+	
+	uint8_t i = 0;
+	uint16_t topic_length = 0;
+	uint16_t total_length = 0;
+	uint8_t x = 0;
+	char topic[50];
+	
+	// init buffer
+	memset( buffer, 0x00, sizeof(buffer) );
+	// init counter
+	length=0;
+	
+	snprintf(topic, sizeof(topic),"channels/%s/publish/%s", channel, apikey);
+	topic_length = strlen( topic );
+	
+	total_length = 2 + topic_length + len;
+	
+	buffer[0] = 48;
+	x = total_length/128;
+	if (x > 0){
+		length++;
+		buffer[length] = (total_length % 128) | 0x80;
+		length++;
+		buffer[length] = x;
+	}else{
+		length++;
+		buffer[length] = total_length;
+	}
+	
+	length++;
+	buffer[length] = 0;
+	length++;
+	buffer[length] = topic_length;
+	length++;
+	
+	//topic
+	for (i = 0; i < topic_length ; i++){
+		buffer[length] = topic[i];
+		length++;
+	}
+	
+	//body
+	for (i = 0; i < len ; i++){
+		buffer[length] = body[i];
+		length++;
+	}
+}
+
+void FrameMQTT::publicar(const char* topic, const char* body)
+{
+	uint8_t x = 0;
+	uint8_t len_topic = strlen(topic);
+	uint16_t len_body = strlen(body);
+	uint16_t len_total = 2 + len_topic + len_body;
+	
+	// init buffer
+	memset( buffer, 0x00, sizeof(buffer) );
+	// init counter
+	length=0;
+	
+	buffer[length] = 48;
+	length++;
+	x = len_total/128;
+	if (x > 0)
+	{	
+		buffer[length] = (len_total % 128) | 0x80;
+		length++;
+		buffer[length] = x;
+		length++;
+	}
+	else
+	{
+		buffer[length] = len_total;
+		length++;
+	}
+	
+	buffer[length] = 0;
+	length++;
+	buffer[length] = len_topic;
+	length++;
+	
+	//topic
+	for (x = 0; x < len_topic ; x++){
+		buffer[length] = topic[x];
+		length++;
+	}
+	
+	//body
+	for (x = 0; x < len_body ; x++){
+		buffer[length] = body[x];
+		length++;
+	}
+	
+}
+
+void FrameMQTT::desconectar(void) 
+{
+	// init buffer
+	memset( buffer, 0x00, sizeof(buffer) );
+	// init counter
+	length=0;
+	
+	buffer[0] = 0xE0;
+	buffer[1] = 0x00;
+	length = 2;
+
+}
+/* 
+void FrameMQTT::showMQTT(void) 
+{
+	
+	for(int i = 0; i <31 ; i++)
+	{
+		USB.print( '=');
+	}
+	USB.println();
+	
+	USB.print(F("Length: "));
+	USB.println(length);
+	
+	USB.print(F("Message (STR): "));
+	USB.println(buffer,length);
+	for(int i = 0; i <31 ; i++)
+	{
+		USB.print( '=');
+	}
+	USB.println();
+} 
+*/
+
+FrameMQTT MQTT = FrameMQTT();
---
+++ FrameMQTT.h
@@ -0,0 +1,106 @@
+/*
+ *
+ * 
+ *  WaspMQTTClient.h
+ *
+ *  The WaspMQTTClient library implements the MQTT protocol to work
+ *  with the waspmote's 3G module. There were no such implementation
+ *  for waspmote, although the MQTT protocol is a very good solution,
+ *  especially for remote, low energy cost applications with unreliable
+ *  network connection.
+ *
+ *  Version:		2.0.0
+ *
+ *  Design:		Glaros Anastasios (glrs)
+ *
+ *  Implementation:	Glaros Anastasios (glrs)
+ *
+ */
+
+
+#ifndef FrameMQTT_h
+#define FrameMQTT_h
+
+//includes
+#include "Arduino.h"
+
+
+/** DEFINE MAX SIZES **/
+#define MQTT_MAX_PACKET_SIZE    150     // Maximum packet size
+//#define MQTT_MAX_TOPIC_SIZE     150     // Maximum topic size
+//#define MQTT_MAX_MESSAGE_SIZE   150     // Maximum message size
+
+
+/** DEFINE KEEP ALIVE VALUE **/
+#define MQTT_KEEPALIVE    300    // keepAlive interval in Seconds
+
+
+/** DEFINED VALUES - MESSAGE TYPE 
+#define MQTT_Reserved     0  // Reserved
+#define MQTT_CONNECT      16  // Client request to connect to Server
+#define MQTT_CONNACK      32  // Connect Acknowledgment
+#define MQTT_PUBLISH      48  // Publish message
+#define MQTT_PUBACK       4 << 4  // Publish Acknowledgment
+#define MQTT_PUBREC       5 << 4  // Publish Received (assured delivery part 1)
+#define MQTT_PUBREL       6 << 4  // Publish Release (assured delivery part 2)
+#define MQTT_PUBCOMP      7 << 4  // Publish Complete (assured delivery part 3)
+#define MQTT_SUBSCRIBE    8 << 4  // Client Subscribe request
+#define MQTT_SUBACK       9 << 4  // Subscribe Acknowledgment
+#define MQTT_UNSUBSCRIBE  10 << 4  // Client Unsubscribe request
+#define MQTT_UNSUBACK     11 << 4  // Unsubscribe Acknowledgment
+#define MQTT_PINGREQ      12 << 4  // PING Request
+#define MQTT_PINGRESP     13 << 4  // PING Response
+#define MQTT_DISCONNECT   224  // Client is Disconnecting
+#define MQTT_Reserved     15 << 4  // Reserved
+
+**/
+
+/** DEFINED VALUES - QoS 
+#define MQTT_QOS0    0 << 1    // At most once (Fire and Forget)
+#define MQTT_QOS1    1 << 1    // At least once (Acknowledged delivery)
+#define MQTT_QOS2    2 << 1    // Exactly once (Assured delivery)
+
+**/
+
+
+class FrameMQTT {
+private:
+	uint8_t _id_max;
+  
+public:
+
+	FrameMQTT();
+	
+	uint8_t  buffer[MQTT_MAX_PACKET_SIZE];
+	uint16_t length;
+	
+	void conectar( const char *id);
+	
+	void conectar( const char *user, const char *key);
+	
+	void conectar( const char *id, const char *user, const char *key);
+	
+	void pub_ThingSpeak(char *channel, char *apikey, char *body);
+	
+	void pub_ThingSpeak(char *channel, char *apikey, uint8_t *body, uint16_t len);
+	
+	void publicar(const char* topic, const char* body);
+	
+	void desconectar(void);
+	
+	//void showMQTT(void);
+ 
+};
+
+extern FrameMQTT MQTT;
+#endif
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
